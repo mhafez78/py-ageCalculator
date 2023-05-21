2a966659a1069c1b40f28b986171bcbde4580546
MOHAMED HAFEZ <mhafez78>
2023-05-20 21:20:09 -0400

    created a cigarettes and alcohol age checker

---
+++ ageChecker.py
@@ -0,0 +1,52 @@
+
+# Python3 code to  calculate age in years
+from datetime import date
+ 
+def calculateAge(born):
+    today = date.today()
+    try:
+        birthday = born.replace(year = today.year)
+ 
+    # raised when birth date is February 29
+    # and the current year is not a leap year
+    except ValueError:
+        birthday = born.replace(year = today.year,
+                  month = born.month + 1, day = 1)
+ 
+    if birthday > today:
+        return today.year - born.year - 1
+    else:
+        return today.year - born.year
+         
+# Driver code
+
+euyy = input('Year of Birth (xxxx): ')
+euyy = int(euyy)
+
+eumm= input('Enter Month of Birth (xx): ')
+eumm = int(eumm)
+
+eudd = input("Enter Day of Birth (xx): ")
+eudd = int(eudd)
+
+
+
+
+print("You are ", calculateAge(date(euyy, eumm, eudd)), "years")
+
+
+def canBuyCigsOrAlcohol():
+    eu_age = calculateAge(date(euyy, eumm, eudd))
+    if eu_age > 18:
+        answer = 'you can buy cigs'
+    if eu_age > 21:
+        answer = 'you can buy cigs and alcohol'
+    else: answer = "you cannot buy either cigs nor alcohol"
+    
+    return answer
+
+print(canBuyCigsOrAlcohol())
+
+
+
+    
